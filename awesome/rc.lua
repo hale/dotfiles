aca073f9e513d15c7c88c786ddc8e67fec88b6a1
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-11-14 20:40:07 +0000

    adding script to setup awesomewm

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -40,7 +40,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "tmux"
+terminal = "gnome-terminal"
 --editor = os.getenv("EDITOR") or "editor"
 editor = 'gvim'
 editor_cmd = terminal .. " -e " .. editor
