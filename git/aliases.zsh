a2e0151dafe588d9ae51eb485a4b075d616c6842
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-02-02 15:03:28 +0000

    fix gc gt commit

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,7 +2,7 @@
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 #alias gp='git push origin HEAD'
 alias gd='git diff'
-alias gc='git commit -m'
+alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
