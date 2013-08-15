beb614d5539cfec13fe09a197724e5f5f7ffd991
Philip Hale <philip@freeagent.com>
2013-08-15 10:13:04 +0100

    Git config changes

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,6 +2,7 @@
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gdc="git diff --cached"
 alias gc='git commit --verbose'
 alias gca='git commit --verbose -a'
 alias gco='git checkout'
