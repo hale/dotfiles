78bc23c3f95930b71ec3938eb40ef909f5c46a16
Philip Hale <philip@freeagent.com>
2013-08-29 01:13:18 +0100

    gp is simply git push, not git push origin HEAD

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,6 @@
 #alias gl='git pull --prune'
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push'
 alias gd='git diff'
 alias gdc="git diff --cached"
 alias gc='git commit --verbose'
