6823a856c300c5aae98b9f1999c63ce84d86574d
Philip Hale <phil989@gmail.com>
2013-09-12 08:35:37 +0100

    Remove a bunch of stuff i never use

--- java/maven-colourise.zsh
+++
@@ -1,9 +0,0 @@
-# Colorize Maven Output
-color_maven() {
-  /usr/local/bin/mvn $* | sed -e 's/Tests run: \([^,]*\), Failures: \([^,]*\), Errors: \([^,]*\), Skipped: \([^,]*\)/\033[1;32mTests run: \1\033[0m, Failures: \033[1;31m\2\033[0m, Errors: \033[1;33m\3\033[0m, Skipped: \033[1;34m\4\033[0m/g' \
-        -e 's/\(\[WARN\].*\)/\033[1;33m\1\033[0m/g' \
-            -e 's/\(\[INFO\].*\)/\033[1;34m\1\033[0m/g' \
-                -e 's/\(\[ERROR\].*\)/\033[1;31m\1\033[0m/g'
-}
-#FIXME: doesn't work
-#alias mvn=color_maven
