f7664383ecf8c5910a8d3f45fb1efb7095cfcc8e
Philip Hale <philip@pghale.com>
2015-06-06 23:37:53 +0100

    Add some anaconda, ipython aliases

---
+++ python/aliases.zsh
@@ -0,0 +1,9 @@
+# Load the anaconda environment (like a virtualenv)
+# e.g. `sa accela_config`
+alias sa='source activate'
+
+# unload the virtualenv
+alias sd='source deactivate'
+
+# launch ipython notebook
+alias ipyn='ipython notebook'
