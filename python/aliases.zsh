c12cd7fb2bcab2829435f4510e7478fca01f8a55
Philip Hale <philip@pghale.com>
2015-06-09 14:11:23 +0100

    Python aliases - anaconda

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -7,3 +7,14 @@ alias sd='source deactivate'
 
 # launch ipython notebook
 alias ipyn='ipython notebook'
+
+# list the available anaconda environments
+alias cl='conda env list'
+
+# create a list of packages installed in this env
+alias clex='conda list --export > exported_packages.txt'
+
+# ccf <env_name> to create a new conda environment using the list
+ccf() {
+  conda create -n $1 --file exported_packages.txt
+}
