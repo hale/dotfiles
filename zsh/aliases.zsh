0661d5455e448e6f24453b7bdb2a3f8703b81533
Philip Hale <philip@pghale.com>
2016-02-13 01:37:20 +0000

    Move zsh aliases into separate file

---
+++ zsh/aliases.zsh
@@ -0,0 +1,14 @@
+alias zshrc="vim ~/.zshrc"
+alias vimrc="vim ~/.vimrc"
+alias todo="vim ~/Dropbox/todo_`timestamp`.markdown"
+alias q="exit"
+
+# shortcut to the tree script. WARN: may conflict with todo.txt
+alias t='tree'
+
+alias ft02="ssh phale@192.168.1.95"
+
+# oopsies
+vom() { cat ~/.dotfiles/zsh/vom.txt }
+alias cim="vim"
+alias :q="exit"
